01d74fc3580c31f973a50974e3178db5c0b90787
venki <venki>
2016-08-15 14:24:37 +0530

    css changes

--- app/scripts/controllers/dashboardController.js
+++ app/scripts/controllers/dashboardController.js
@@ -44,37 +44,138 @@ angular.module('reverApp.controllers')
         }
         /*$scope.myChartObject = {};
 
-        $scope.myChartObject.type = "LineChart";
+         $scope.myChartObject.type = "LineChart";
 
-        $scope.onions = [
-            {v: "Onions"},
-            {v: 3},
+         $scope.onions = [
+         {v: "Onions"},
+         {v: 3},
+         ];
+
+         $scope.myChartObject.data = {"cols": [
+         {id: "t", label: "Topping", type: "date"},
+         {id: "s", label: "Slices", type: "number"}
+         ], "rows": [
+         {c: [
+         {v: "new Date(2314, 2, 15)"},
+         {v: 3},
+         ]},
+         {c: $scope.onions},
+         {c: [
+         {v: "new Date(2314, 2, 15)"},
+         {v: 31}
+         ]},
+         {c: [
+         {v: "new Date(2314, 2, 15)"},
+         {v: 1},
+         ]},
+         {c: [
+         {v: "new Date(2314, 2, 15)"},
+         {v: 2},
+         ]}
+         ]};
+
+         $scope.myChartObject.options = {
+         'title': 'How Much Pizza I Ate Last Night'
+         };*/
+
+
+        $scope.today = function() {
+            $scope.startDate = new Date();
+            $scope.endDate = new Date();
+        };
+        $scope.today();
+
+        $scope.clear = function() {
+            $scope.startDate = null;
+            $scope.endDate = null;
+        };
+
+        $scope.inlineOptions = {
+            customClass: getDayClass,
+            minDate: new Date(),
+            showWeeks: true
+        };
+
+        $scope.dateOptions = {
+            dateDisabled: disabled,
+            formatYear: 'yy',
+            maxDate: new Date(),
+            minDate: new Date(),
+            startingDay: 1
+        };
+
+        // Disable weekend selection
+        function disabled(data) {
+            var date = data.date,
+                mode = data.mode;
+            return false;
+        }
+
+        $scope.toggleMin = function() {
+            $scope.inlineOptions.minDate = $scope.inlineOptions.minDate ? null : new Date();
+            $scope.dateOptions.minDate = $scope.inlineOptions.minDate;
+        };
+
+        $scope.toggleMin();
+
+        $scope.open1 = function() {
+            $scope.popup1.opened = true;
+        };
+
+        $scope.open2 = function() {
+            $scope.popup2.opened = true;
+        };
+
+        $scope.setDate = function(year, month, day) {
+            $scope.startDate = new Date(year, month, day);
+            $scope.endDate = new Date(year, month, day);
+        };
+
+        $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+        $scope.format = $scope.formats[0];
+        $scope.altInputFormats = ['M!/d!/yyyy'];
+
+        $scope.popup1 = {
+            opened: false
+        };
+
+        $scope.popup2 = {
+            opened: false
+        };
+
+        var tomorrow = new Date();
+        tomorrow.setDate(tomorrow.getDate() + 1);
+        var afterTomorrow = new Date();
+        afterTomorrow.setDate(tomorrow.getDate() + 1);
+        $scope.events = [
+            {
+                date: tomorrow,
+                status: 'full'
+            },
+            {
+                date: afterTomorrow,
+                status: 'partially'
+            }
         ];
 
-        $scope.myChartObject.data = {"cols": [
-            {id: "t", label: "Topping", type: "date"},
-            {id: "s", label: "Slices", type: "number"}
-        ], "rows": [
-            {c: [
-                {v: "new Date(2314, 2, 15)"},
-                {v: 3},
-            ]},
-            {c: $scope.onions},
-            {c: [
-                {v: "new Date(2314, 2, 15)"},
-                {v: 31}
-            ]},
-            {c: [
-                {v: "new Date(2314, 2, 15)"},
-                {v: 1},
-            ]},
-            {c: [
-                {v: "new Date(2314, 2, 15)"},
-                {v: 2},
-            ]}
-        ]};
+        function getDayClass(data) {
+            var date = data.date,
+                mode = data.mode;
+            if (mode === 'day') {
+                var dayToCheck = new Date(date).setHours(0,0,0,0);
 
-        $scope.myChartObject.options = {
-            'title': 'How Much Pizza I Ate Last Night'
-        };*/
+                for (var i = 0; i < $scope.events.length; i++) {
+                    var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
+
+                    if (dayToCheck === currentDay) {
+                        return $scope.events[i].status;
+                    }
+                }
+            }
+
+            return '';
+        }
+
+
+
     }]);
